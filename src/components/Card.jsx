2cfbb8869c06af1d499e7de6d268838f81affccf
shreedhar-aib <shreedhar@aibudhha.in>
2024-05-04 14:46:00 +0530

    project setup

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = ({ data }) => {
+  console.log(data);
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h2>title</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
